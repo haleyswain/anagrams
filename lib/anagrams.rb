dddfa79481a590b092871c6a8dbc9de66cf42dab
Haley Swain and Jonathan Thom <haley@>
2016-11-17 10:51:51 -0800

    Will recognize matches and non matches for multicharacter words

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,6 +1,9 @@
 class String
   define_method(:anagrams) do |guess|
-    if self == guess
+    word_array = self.split("")
+    guess_array = guess.split("")
+
+    if word_array.sort == guess_array.sort
       guess + " is an anagram of " + self
     else
       guess + " is not an anagram of " + self
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -10,4 +10,12 @@ describe("String#anagrams") do
   it ('will recognize if a single character word is not an anagram, for one guess') do
     expect('a'.anagrams('b')).to(eq("b is not an anagram of a"))
   end
+
+  it('will recognize if a multi character word is an anagram') do
+    expect('cat'.anagrams('act')).to(eq('act is an anagram of cat'))
+  end
+
+  it('will recognize if a multi character word is not an anagram') do
+    expect('cat'.anagrams('butt')).to(eq('butt is not an anagram of cat'))
+  end
 end
