ac4bde694950604d542e52bae5019083a018ce38
Haley Swain and Jonathan Thom <jonathan@>
2016-11-17 11:24:30 -0800

    Will compare original string to multiple different inputs

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,12 +1,16 @@
 class String
   define_method(:anagrams) do |guess|
+    guesses = guess.split(", ")
     word_array = self.delete(" ").split("")
-    guess_array = guess.delete(" ").split("")
-
-    if word_array.sort == guess_array.sort
-      guess + " is an anagram of " + self
-    else
-      guess + " is not an anagram of " + self
+    output = ""
+    guesses.each() do |possible|
+      possible_array = possible.delete(" ").split("")
+      if word_array.sort == possible_array.sort
+        output += possible + " is an anagram of " + self + "."
+      else
+        output += possible + " is not an anagram of " + self + "."
+      end
     end
+    output
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,26 +4,34 @@ require('anagrams')
 
 describe("String#anagrams") do
   it ('will recognize if single character words are anagrams, for one guess') do
-    expect("a".anagrams("a")).to(eq("a is an anagram of a"))
+    expect("a".anagrams("a")).to(eq("a is an anagram of a."))
   end
 
   it ('will recognize if a single character word is not an anagram, for one guess') do
-    expect('a'.anagrams('b')).to(eq("b is not an anagram of a"))
+    expect('a'.anagrams('b')).to(eq("b is not an anagram of a."))
   end
 
   it('will recognize if a multi character word is an anagram') do
-    expect('cat'.anagrams('act')).to(eq('act is an anagram of cat'))
+    expect('cat'.anagrams('act')).to(eq('act is an anagram of cat.'))
   end
 
   it('will recognize if a multi character word is not an anagram') do
-    expect('cat'.anagrams('butt')).to(eq('butt is not an anagram of cat'))
+    expect('cat'.anagrams('butt')).to(eq('butt is not an anagram of cat.'))
   end
 
   it('will recognize if a multiple word string is an anagram') do
-    expect('cat dog'.anagrams('dog ca t')).to(eq('dog ca t is an anagram of cat dog'))
+    expect('cat dog'.anagrams('dog ca t')).to(eq('dog ca t is an anagram of cat dog.'))
   end
 
   it('will recognize if a multiple word string is not an anagram') do
-    expect('cat dog'.anagrams('dog butt')).to(eq('dog butt is not an anagram of cat dog'))
+    expect('cat dog'.anagrams('dog butt')).to(eq('dog butt is not an anagram of cat dog.'))
+  end
+
+  it('Will compare original string to multiple anagrams and recognize matches') do
+    expect('cat'.anagrams('act, tac')).to(eq('act is an anagram of cat.tac is an anagram of cat.'))
+  end
+
+  it('Will compare original string to multiple anagrams and recognize non matches') do
+    expect('cat'.anagrams('act, butt')).to(eq('act is an anagram of cat.butt is not an anagram of cat.'))
   end
 end
